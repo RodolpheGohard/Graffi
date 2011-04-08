63db607a39de6982fe0724c7b0a1b18b6e36e288
Rodolphe Gohard <iscope@iscope-EasyNote-MB87.(none)>
2011-04-08 18:15:52 +0200

    implementing skeletton of Graffi.Pie #1

--- graffi.pie.js
+++ graffi.pie.js
@@ -1,11 +1,43 @@
-/**
- * @author Rodolphe Gohard
- */
 
 (function() {
 	
+	/**
+	 * @author Rodolphe Gohard
+	 * @class Pie
+	 */
 	Graffi.Pie = function( holder, iterator, parameters ) {
+		var shares = [], values=[];
+		var total = 0;
+		var currentAngle = Math.PI/2;
+		var current;
+		var i,l;
 		
-	}
+		//We get the total
+		while( current = iterator.next() ) {
+			total += current;
+			values.push( current );
+		}
+		
+		//Maybe some ordering here
+		
+		//We compute the relative shares
+		for ( i=0,l=values.length ; i<l ; i++ ) {
+			shares[i] = values[i]/total;
+			
+			this.portionRenderer.drawPortion( currentAngle, shares[i] );
+			
+		}
+		
+		
+		
+		
+	};
+	Graffi.Pie.prototype = new Graffi.Chart();
+	
+	Graffi.Pie.prototype.portionRenderer = {
+		drawPortion: function( startAngle, amount ) {
+			
+		}	
+	};
 	
 })();
