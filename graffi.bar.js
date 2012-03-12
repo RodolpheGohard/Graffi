cd1bb9a9ec17eb837561558bf09a85dde917468a
Rodolphe Gohard <rodolphe.gohard@iscope.fr>
2012-03-12 17:39:54 +0100

    added condition to avoid less-that-0 sized bars

--- graffi.bar.js
+++ graffi.bar.js
@@ -59,11 +59,12 @@
 	Graffi.Bar.prototype.drawer = {
 			drawBar: function( holder, x, y, w, h, color ) {
 				var returnv = holder.rect(x,y,w,h,5).attr({color:color,fill:color});
-				holder.rect(x+1,y+1,w/4-2,h-2,5).attr({stroke:'none',fill:'White',opacity:0.1});
-				holder.rect(x+3*w/4,y+1,w/4-2,h-2,5).attr({stroke:'none',fill:'Black',opacity:0.1});
+				var cleanHeight = Math.max( h-2, 0 );
+				holder.rect( x+1, y+1, w/4-2, cleanHeight, 5).attr({stroke:'none',fill:'White',opacity:0.1});
+				holder.rect( x+3*w/4, y+1, w/4-2, cleanHeight, 5).attr({stroke:'none',fill:'Black',opacity:0.1});
 				return returnv;
 			}
-	}
+	};
 	
 	
 	
@@ -79,7 +80,7 @@
 	 */
 	Graffi.MultiBar = function( holder, iterator, parameters ) {
 		var ymax=10,
-			xmax=iterator.get(0).iterator.length
+			xmax=iterator.get(0).iterator.length,
 			xscale,
 			yscale;
 		
@@ -97,7 +98,7 @@
 		var yscale = this.height /ymax;
 		
 		this.drawBars( 40, this.height+10, this.width, this.height, xscale, yscale );
-	}
+	};
 	
 	Graffi.MultiBar.prototype = new Graffi.Bar();
 	/**
